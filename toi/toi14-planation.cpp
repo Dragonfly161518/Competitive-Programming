7a4282565eea3f844de8e1928ca0849a753e4566
dragonfly161518 <tatsuya10199@gmail.com>
2019-06-03 14:55:20 +0700

    comple closest pair in toi14 plantation but not for problem

--- toi/toi14-planation.cpp
+++ toi/toi14-planation.cpp
@@ -21,7 +21,21 @@ int closetpair(int l,int r) {
     int d2 = closetpair(m+1,r);
 
     int d = min(d1,d2);
-
+    vector<float,int> > v;
+    for(int i=0;i<n;i++) {
+        if(abs(p[m]-p[i]) < d) {
+            v.push_back({p[i].second,i});
+        }
+    }
+    sort(v.begin(),v.end());
+    for(int i=0;i<v.size();i++) {
+        for(int j=i+1;j<v.size();j++)
+            if(abs(p[v[i]].second-p[v[j]].second) < d) {
+                d = min(d,cal(i,j));
+            }
+            else break;
+    }
+    return d;
 }
 
 int main() {
